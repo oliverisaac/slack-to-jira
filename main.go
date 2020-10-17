05996d6b9361aea2fc50e5a407fd703967d096cf
Oliver Isaac <oisaac@gmail.com>
2020-10-16 20:55:08 -0500

    added handling of specific users

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/alexflint/go-arg"
 	"github.com/pkg/errors"
@@ -19,13 +20,15 @@ type slackListener struct {
 	client            *slack.Client
 	EmojiName         string
 	userCache         map[string]*slack.User
+	userJiraPairs     map[string]string
 }
 
 func newSlackListener(slackToken string) *slackListener {
 	sl := slackListener{
-		Token:     slackToken,
-		client:    slack.New(slackToken),
-		userCache: make(map[string]*slack.User),
+		Token:         slackToken,
+		client:        slack.New(slackToken),
+		userCache:     make(map[string]*slack.User),
+		userJiraPairs: make(map[string]string),
 	}
 	return &sl
 }
@@ -68,10 +71,19 @@ func (sl *slackListener) handler(w http.ResponseWriter, r *http.Request) {
 		case *slackevents.AppMentionEvent:
 			sl.client.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
 		case *slackevents.ReactionAddedEvent:
-			log.Tracef("Received reaction, channel: %s, reaction: %s, user: %s", ev.Item.Channel, ev.Reaction, ev.User)
-			if ev.Reaction != sl.EmojiName {
+			log.Tracef("Received reaction, channel: %s, reaction: %s, user: %s, item type: %s", ev.Item.Channel, ev.Reaction, ev.User, ev.Item.Type)
+			if ev.Reaction != sl.EmojiName && ev.Item.Type != "message" {
 				return
 			}
+
+			messageRef := slack.ItemRef{
+				Channel:   ev.Item.Channel,
+				Timestamp: ev.Item.Timestamp,
+			}
+
+			sl.client.AddReaction("hourglass_flowing_sand", messageRef)
+			defer sl.client.RemoveReaction("hourglass_flowing_sand", messageRef)
+
 			user, ok := sl.userCache[ev.User]
 			if !ok {
 				user, err = sl.client.GetUserInfo(ev.User)
@@ -81,7 +93,7 @@ func (sl *slackListener) handler(w http.ResponseWriter, r *http.Request) {
 				}
 				sl.userCache[ev.User] = user
 			}
-			log.Debugf("Got actionable reaction %s from user %s", ev.Reaction, user.Name)
+			log.Debugf("Got actionable reaction %s from user %s", ev.Reaction, user.Profile.Email)
 		default:
 			log.Info("Received unexpected innerevent: " + innerEvent.Type)
 		}
@@ -103,6 +115,7 @@ func main() {
 		SlackToken             string `arg:"-s,--slack-token,env:SLACK_TOKEN" help:"Slack auth token"`
 		SlackVerificationToken string `arg:"-f,--slack-verification-token,env:SLACK_VERIFICATION_TOKEN" default:"" help:"Slack verification token"`
 		JiraToken              string `arg:"-j,--jira-token,env:JIRA_TOKEN" help:"Jira auth token"`
+		UserJiraPairs          string `arg:"-u,--user-jira-pairs,env:USER_JIRA_PAIRS" help:"Comma separated list of email/jira project pairs. For example: user@example.com=SYS,bob@example.com=PROJ`
 	}
 	arg.MustParse(&args)
 
@@ -124,6 +137,15 @@ func main() {
 	sl := newSlackListener(args.SlackToken)
 	sl.VerificationToken = args.SlackVerificationToken
 	sl.EmojiName = args.EmojiName
+
+	for _, ujp := range strings.Split(args.UserJiraPairs, ",") {
+		ujpArr := strings.Split(ujp, "=")
+		if len(ujpArr) != 2 {
+			log.Error("Invalid user-jira-pair: " + ujp)
+			continue
+		}
+		sl.userJiraPairs[ujpArr[0]] = ujpArr[1]
+	}
 
 	http.HandleFunc("/slack", sl.handler)
 	http.HandleFunc("/health", sl.healthHandler)
